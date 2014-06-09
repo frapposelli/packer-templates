335dbfa489ee250a89737f7b122566960e0a189b
Fabio Rapposelli <fabio@rapposelli.org>
2014-06-10 00:29:09 +0800

    🚀 General consolidation and added Windows template borrowed from packer-windows

---
+++ scripts/windows-vmware-tools_install.bat
@@ -0,0 +1,13 @@
+if not exist "C:\Windows\Temp\7z920-x64.msi" (
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/sevenzip/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
+)
+msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
+
+if exist "C:\Users\vagrant\windows.iso" (
+    move /Y C:\Users\vagrant\windows.iso C:\Windows\Temp
+)
+
+cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Windows\Temp\windows.iso" -oC:\Windows\Temp\VMWare"
+cmd /c C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
+
+msiexec /qb /x C:\Windows\Temp\7z920-x64.msi
