afaf693d906f96832b1286e02be96ca3fe53ff71
Fabio Rapposelli <fabio@rapposelli.org>
2014-08-11 12:02:02 +0200

    🌠 Cleanup and refactoring, added cleanup scripts for CentOS

--- scripts/ubuntu-vmware-vcloud_cleanup.sh
+++ scripts/ubuntu-vmware-vcloud_cleanup.sh
@@ -79,5 +79,5 @@ dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
 # Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
+# Packer might quit too early before the large files are deleted
 sync
