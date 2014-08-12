735c39e03d620527079304a7e20f6dfb2a3b852c
Fabio Rapposelli <fabio@rapposelli.org>
2014-08-12 19:04:33 +0200

    puppet script fix

--- scripts/ubuntu-puppet_install.sh
+++ scripts/ubuntu-puppet_install.sh
@@ -8,3 +8,4 @@ wget https://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb
 dpkg -i puppetlabs-release-${DISTRIB_CODENAME}.deb
 rm puppetlabs-release-${DISTRIB_CODENAME}.deb
 apt-get update
+apt-get install puppet
