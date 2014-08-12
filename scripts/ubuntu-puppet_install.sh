e55bf684db5ea9594f281bac5b6084fdaa84b105
Fabio Rapposelli <fabio@rapposelli.org>
2014-08-12 19:10:28 +0200

    typo

--- scripts/ubuntu-puppet_install.sh
+++ scripts/ubuntu-puppet_install.sh
@@ -8,4 +8,4 @@ wget https://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb
 dpkg -i puppetlabs-release-${DISTRIB_CODENAME}.deb
 rm puppetlabs-release-${DISTRIB_CODENAME}.deb
 apt-get update
-apt-get install puppet
+apt-get install -y puppet
