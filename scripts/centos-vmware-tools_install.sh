e4b6254edf38ebe88fd24a043313ad7298474581
Fabio Rapposelli <fabio@rapposelli.org>
2014-08-12 18:34:51 +0200

    - eliminated several ubuntu releases - fixed puppet install - attempt at fixing centos 7 install

--- scripts/centos-vmware-tools_install.sh
+++ scripts/centos-vmware-tools_install.sh
@@ -11,9 +11,3 @@ cd /home/vagrant
 rm -rf vmware-tools-distrib
 rm -f VMwareTools*.tar.gz
 rm linux.iso
-
-# Fix network
-sudo ifdown eth0
-sudo sed -i '/^HWADDR=.*$/d' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo ifup eth0
-sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
---
+++ scripts/ubuntu-puppet_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. /etc/lsb-release
+
+echo "==> OS detected: ${DISTRIB_ID} ${DISTRIB_RELEASE} (${DISTRIB_CODENAME})"
+echo "==> Setting up puppet from apt.puppetlabs.com"
+wget https://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb
+dpkg -i puppetlabs-release-${DISTRIB_CODENAME}.deb
+rm puppetlabs-release-${DISTRIB_CODENAME}.deb
+apt-get update
